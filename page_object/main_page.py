311968392845a9734270b83c55106e6d60a880ea
lastpagevlad <116770372+lastpagevlad@users.noreply.github.com>
2023-05-25 20:34:10 +0300

    Добавление файлов

--- page_object/main_page.py
+++ page_object/main_page.py
@@ -3,4 +3,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pytest
+from .base_page import BasePage
 
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
